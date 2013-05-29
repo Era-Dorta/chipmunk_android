a2bf6b01266021b07642fb73767bfd319b81b14d
neochuky <neochuki@gmail.com>
2013-05-29 10:49:40 +0100

    Bumped to version 0.0.2.dev Added array access operator do Vec2 class

--- lib/chipmunk.rb
+++ lib/chipmunk.rb
@@ -2,6 +2,15 @@ module CP
 	java_import 'chipmunk.java.Shape'
 	java_import 'chipmunk.java.Space'
 	java_import 'chipmunk.java.Vec2'
+	
+	#Add array access method to Vec2 here since 
+	#Java does not allow it on its side
+	class Vec2
+    def [](index)
+      @coordinates[index]
+    end
+	end
+	
 	java_import 'chipmunk.java.Body'
 	
 	class Shape
